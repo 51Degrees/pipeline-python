c8e90d5960af2ae05c8f179d2fec56668e977723
Oleksandr Lazarenko <oleksandr.lazarenko@postindustria.com>
2026-07-03 19:14:06 +0200

    BUILD: Register fiftyone_pipeline_did in CI scripts and add its tox.ini.

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -3,7 +3,7 @@ param (
     [string]$RepoName
 )
 
-$packages = "fiftyone_pipeline_core", "fiftyone_pipeline_engines", "fiftyone_pipeline_engines_fiftyone"
+$packages = "fiftyone_pipeline_core", "fiftyone_pipeline_engines", "fiftyone_pipeline_engines_fiftyone", "fiftyone_pipeline_did"
 ./python/run-unit-tests.ps1 -RepoName $RepoName -Packages $packages
 
 exit $LASTEXITCODE
